98d2229ae4f07ccf0f774e882c61349d82e57036
ANANYA N M <161317978+ANANYANM@users.noreply.github.com>
2024-08-29 13:29:46 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,40 +1,34 @@
 using BlazorApp2;
 using BlazorApp2.Components;
 using BlazorApp2.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-// Add services to the container.
 builder.Services.AddSingleton<TicketService>();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped<ICustomAuthenticationService, CustomAuthenticationService>();
-builder.Services.AddAuthentication("Cookies")
-    .AddCookie("Cookies", options =>
-    {
-        options.LoginPath = "/customer-login";
-    });
-
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<AuthenticationService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseAuthorization();
-
 app.UseAntiforgery();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
 app.Run();
