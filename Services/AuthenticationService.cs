168a091e4aec77a8430f958e77746c83e96b20c2
ANANYA N M <161317978+ANANYANM@users.noreply.github.com>
2024-08-29 13:24:42 +0530

    Add files via upload

---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp2.Services;
+
+public class AuthenticationService
+{
+    public async Task<bool> IsValidUser(string username, string password)
+    {
+        await Task.Delay(100); // Simulate some asynchronous operation.
+        return true;
+    }
+}
+
