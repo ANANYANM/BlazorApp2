254ea1c9b0c60c9e40d9ac5ec3b460486e82f3a2
ANANYA N M <161317978+ANANYANM@users.noreply.github.com>
2024-08-30 22:48:32 +0530

    Add files via upload

---
+++ Models/GlobalVariables.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp2.Models
+{
+    public class GlobalVariables
+    {
+        // static type to be accessed from anywhere
+        public static bool isAdmin = false; // for checking admin
+        public static bool isCustomer = false; // for checking customer
+    }
+}
+
