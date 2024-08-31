bccceb2027f3529186a9aeed93d45f44fe1bcadf
ANANYA N M <161317978+ANANYANM@users.noreply.github.com>
2024-08-31 07:38:14 +0530

    Update CustomAuthenticationService.cs

--- CustomAuthenticationService.cs
+++ CustomAuthenticationService.cs
@@ -3,8 +3,6 @@ public class CustomAuthenticationService : ICustomAuthenticationService
     {
         public Task<bool> LoginAsync(string username, string password)
         {
-            // Implement your login logic here
-            // Return true if login is successful, otherwise false
             return Task.FromResult(username == "admin" && password == "password"); // Example logic
         }
 
