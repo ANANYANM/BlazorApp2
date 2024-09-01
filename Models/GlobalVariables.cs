94b9647add3e82b8bbc6cd6f7c3a71dfd0670d62
ANANYA N M <161317978+ANANYANM@users.noreply.github.com>
2024-09-01 10:11:33 +0530

    Update GlobalVariables.cs

--- Models/GlobalVariables.cs
+++ Models/GlobalVariables.cs
@@ -5,6 +5,7 @@
         // static type to be accessed from anywhere
         public static bool isAdmin = false; // for checking admin
         public static bool isCustomer = false; // for checking customer
+        public static bool isAgent = false; // for checking agent
     }
 }
 
