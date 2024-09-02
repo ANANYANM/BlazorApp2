afe9d3454b02b87ba74a7f81c8e9cad22c88701d
ANANYA N M <161317978+ANANYANM@users.noreply.github.com>
2024-09-02 09:56:15 +0530

    Update TicketService.cs

--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -14,7 +14,26 @@ namespace BlazorApp2.Services
         {
             tickets.Add(ticket);
         }
+            public void UpdateTicket(Ticket updatedTicket)
+    {
+        // Find the ticket with the same TicketId
+        var ticket = tickets.FirstOrDefault(t => t.TicketId == updatedTicket.TicketId);
+
+        if (ticket != null)
+        {
+            // Update the fields of the existing ticket with the updated ticket details
+            ticket.Title = updatedTicket.Title;
+            ticket.CustomerName = updatedTicket.CustomerName;
+            ticket.Issue = updatedTicket.Issue;
+            ticket.CompanyName = updatedTicket.CompanyName;
+            ticket.IssueCategory = updatedTicket.IssueCategory;
+            ticket.Description = updatedTicket.Description;
+            ticket.CreatedDate = updatedTicket.CreatedDate;
+            ticket.Status = updatedTicket.Status;
+            ticket.AssignTo = updatedTicket.AssignTo;
+        }
     }
+}
 }
 
 
