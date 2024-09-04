245d2971e27629012fbafe2a9b0d5477775c7906
ANANYA N M <161317978+ANANYANM@users.noreply.github.com>
2024-09-04 10:57:50 +0530

    Update GlobalVariables.cs

--- Models/GlobalVariables.cs
+++ Models/GlobalVariables.cs
@@ -1,12 +1,11 @@
-﻿namespace BlazorApp2.Models
+namespace BlazorApp2.Models
 {
     public class GlobalVariables
     {
         // static type to be accessed from anywhere
-        public static bool isAdmin = false; // for checking admin
+        public static bool isAdmin=false; // for checking admin
         public static bool isCustomer = false; // for checking customer
-        public static bool isAgent = false; // for checking agent
+        public static bool isAgent = false;//for checking agent 
         public static string CurrentAgentName { get; set; } = string.Empty;// Default to an empty string
     }
 }
-
