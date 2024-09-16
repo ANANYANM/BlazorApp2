98092a1a32b982d256f261ba1ca3f1acbf746662
ANANYANM <ananyanm166@gmail.com>
2024-09-16 19:09:30 +0530

    cmd

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE TicketingSystem;
+CREATE TABLE Tickets(
+    TicketId INT PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(100),
+    CustomerName NVARCHAR(100),
+    Issue NVARCHAR(500),
+    CompanyName NVARCHAR(100),
+    IssueCategory NVARCHAR(100),
+    Description NVARCHAR(MAX),
+    Status NVARCHAR(50),
+    AssignTo NVARCHAR(100)
+);
+SELECT *FROM Tickets;
