87c44987d1dc2855532125d546cb2f6d96fc55e3
ANANYANM <ananyanm166@gmail.com>
2024-09-17 17:54:01 +0530

    database commit

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -4,14 +4,14 @@
 public class Ticket
 {
     public int TicketId { get; set; }
-    public string Title { get; set; }
-    public string CustomerName { get; set; }
-    public string Issue { get; set; }
-    public string CompanyName { get; set; }
-    public string IssueCategory { get; set; }
-    public string Description { get; set; }
-    public string Status { get; set; }
-    public string AssignTo { get; set; }
+    public string? Title { get; set; }
+    public string? CustomerName { get; set; }
+    public string? Issue { get; set; }
+    public string? CompanyName { get; set; }
+    public string? IssueCategory { get; set; }
+    public string? Description { get; set; }
+    public string? Status { get; set; }
+    public string? AssignTo { get; set; }
 }
 }
 
--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -35,6 +35,3 @@ namespace BlazorApp2.Services
         }
     }
 }
-
-
-
