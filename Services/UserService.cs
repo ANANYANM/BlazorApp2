4657c141fd1ef24b77faa64ac669b95e8b0f877c
ANANYANM <ananyanm166@gmail.com>
2024-10-16 10:22:22 +0530

    commits

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -13,19 +13,9 @@ namespace BlazorApp2.Services
             return _dbContext;
         }
 
-        //    private List<User> Users = new List<User>
-        //{
-        //    new User { Username = "admin", PasswordHash = "password", Role = "admin" },
-        //    new User { Username = "agent1", PasswordHash = "password", Role = "agent" },
-        //    new User { Username = "agent2", Password = "password", Role = "agent" },
-        //    new User { Username = "agent3", Password = "password", Role = "agent" },
-        //    new User { Username = "customer",Password = "password", Role = "customer" }
-        //};
-
         // Fetch users from the database based on username and password
         public async Task<User?> AuthenticateUser(string username, string password)
         {
-            // return Users.FirstOrDefault(u => u.Username == username && u.Password == password);
             return await _dbContext.Users
             .FirstOrDefaultAsync(u => u.Username == username && u.Password == password && u.IsApproved == true);
         }
